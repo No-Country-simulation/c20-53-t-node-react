3cc4343fab4938eb953fce8b7055cc38537776aa
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-09-02 17:21:27 -0400

    fix: fix type params

--- server/src/common/hashing-password.ts
+++ server/src/common/hashing-password.ts
@@ -2,11 +2,11 @@ import { compare, hash } from 'bcrypt'
 
 const SALT = process.env.SALT_ROUNDS
 
-export const hashingPassword = async (password) => {
+export const hashingPassword = async (password: string) => {
   return await hash(password, parseInt(SALT))
 }
 
-export const comparePasswordHashing = async (password, hashingPassowrd) => {
+export const comparePasswordHashing = async (password: string, hashingPassowrd: string) => {
   const correctPassword = await compare(password, hashingPassowrd)
   return correctPassword
 }
