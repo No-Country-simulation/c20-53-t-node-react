bfa20424aa2c9b749368871dad673c5519203fb7
zaidraed <zaidraed@gmail.com>
2024-09-11 21:19:02 -0300

    feat: route order update

--- server/src/order/dto/update-order.dto.ts
+++ server/src/order/dto/update-order.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateOrderDto } from './create-order.dto';
+import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { CreateOrderDto } from "./create-order.dto";
+import { IsString } from "class-validator";
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+  @ApiProperty({
+    example: "66d61e26d8aa181ecd0c34f7",
+    description: "the id of the order",
+    nullable: false,
+  })
+  @IsString()
+  id: string;
+}
--- server/src/order/order.service.ts
+++ server/src/order/order.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateOrderDto } from "./dto/create-order.dto";
 
 import { PrismaService } from "src/prisma/prisma.service";
 import { User } from "src/auth/interfaces";
+import { UpdateOrderDto } from "./dto/update-order.dto";
+import { handleErrorExceptions } from "src/common/handleErrorsExcepcions";
 
 @Injectable()
 export class OrderService {
@@ -22,8 +24,24 @@ export class OrderService {
     return await this.prisma.order.findUnique({ where: { id: String(id) } });
   }
 
-  async update(id) {
-    return await `This action updates a #${id} order`;
+  async update(updateOrderDto: UpdateOrderDto) {
+    try {
+      const order = await this.findOne(updateOrderDto.id);
+      if (!order) {
+        new NotFoundException("Order not found");
+      }
+      if (order.Status !== "PENDING") {
+        new NotFoundException("Order can`t be updated");
+      }
+      return await this.prisma.order.update({
+        where: { id: String(updateOrderDto.id) },
+        data: {
+          Status: updateOrderDto.Status,
+        },
+      });
+    } catch (error) {
+      handleErrorExceptions(error);
+    }
   }
 
   async remove(id: string) {
